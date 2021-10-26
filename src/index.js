3486cbd58c39961ff49e5e3e409e84cb42dd6c9b
Selvakartig <selvakartig@gmail.com>
2021-10-26 23:37:57 +0530

    HashRouter for github pages

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom"; //-- For GITHUB Pages --//
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./index.css";
 
 ReactDOM.render(
-	<BrowserRouter>
+	<Router>
 		<React.StrictMode>
 			<App />
 		</React.StrictMode>
-	</BrowserRouter>,
+	</Router>,
 	document.getElementById("root")
 );
