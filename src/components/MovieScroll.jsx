7553ebe7343b12de444a4dccec65254e4f778c20
Selvakartig <selvakartig@gmail.com>
2022-06-19 23:28:36 +0530

    testing pull

--- src/components/MovieScroll.jsx
+++ src/components/MovieScroll.jsx
@@ -5,7 +5,7 @@ export default function MovieScroll() {
 		<header className="bg-light py-5">
 			<div className="container px-4 px-lg-5 my-5">
 				<div className="text-center text-black">
-					<h1 className="display-4 fw-bolder">Welcome to SED Movies</h1>
+					<h1 className="display-4 fw-bolder">Welcome to SEDsss Movies</h1>
 					<p className="lead fw-normal text-black-50 mb-0">
 						Enjoy your free time with us!
 					</p>
